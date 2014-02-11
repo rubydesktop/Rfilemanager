6db2303647103367b2306d8cfb2190a118ba7575
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2014-02-11 20:44:10 +0200

    fixed volume mount

--- rfilemanager/rfilemanager-file-actions.rb
+++ rfilemanager/rfilemanager-file-actions.rb
@@ -11,9 +11,13 @@ class FileActions
   def get_volumes
     hash = {"File System" => "/"}
     all_volumes = Gio::VolumeMonitor.get.volumes
+    # mount volumes
     all_volumes.each do |volume|
       v = volume.mount
-      hash.store(v.name, v.get_mount.default_location.path)
+    end
+    # get volumes
+    all_volumes.each do |volume|
+      hash.store(volume.mount.name, volume.mount.get_mount.default_location.path)
     end
     return hash
   end
